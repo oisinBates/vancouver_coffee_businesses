309f08d6733bcf9cc592626d37038f91e64eaf0a
oisinBates <bates.oisin@gmail.com>
2023-08-20 15:57:24 -0700

    modifying API search string, dataframe logic, and variable names

--- source/analytics.R
+++ source/analytics.R
@@ -11,12 +11,22 @@ coffee_keywords <-
     "'McDonalds*'",
     "'Starbucks*'",
     "'Tim Horton*'")
+
 api_where_string <-
-  str_c(coffee_keywords, collapse = " OR businesstradename LIKE ")
-active_businesses <-
+  paste0(
+    "status='Issued' AND (",
+    str_c(coffee_keywords, collapse = " OR businesstradename LIKE "),
+    ")"
+  )
+
+coffee_businesses <-
   VancouvR::get_cov_data(dataset_id = "business-licences", where = api_where_string)
 
-coffee_franchises <- active_businesses %>%
+
+# Starbucks has premises registered under the business types of `Wholesale  Dealer` and `Instruction` at `2930 VIRTUAL WAY`.
+# JJ Bean had a premises registered from 2013 to 2018 registered under the business types of `Manufacturer - Food` at `460 RAILWAY ST`.
+# Since these business types are not customer facing, they are filtered out of this dataframe.
+coffee_chains <- coffee_businesses %>%
   mutate(
     formatted_tradename = case_when(
       grepl("Blenz", businesstradename) ~ "blenz",
@@ -26,8 +36,7 @@ coffee_franchises <- active_businesses %>%
       grepl("Starbucks", businesstradename) ~ "starbucks",
       grepl("Tim Hortons", businesstradename) ~ "tim_hortons",
       grepl("Tim Horton's", businesstradename) ~ "tim_hortons"
-    ),
-    licenseyear = substr(expireddate, 0, 4)
+    )
   ) %>%
   filter(
     formatted_tradename %in% c("blenz",
@@ -35,37 +44,61 @@ coffee_franchises <- active_businesses %>%
                                "mcdonalds",
                                "starbucks",
                                "tim_hortons")
-  )
+  ) %>%
+  filter(businesstype %in% c("Ltd Service Food Establishment",
+                             "Restaurant Class 1")) %>%
+  rename("year" = "folderyear")
 
 
-# How many businesses are closing and open in each year
-license_status_count_by_year <- coffee_franchises %>%
-  group_by(formatted_tradename, folderyear, status) %>%
-  tally()
+# Manually adding 'localarea' for the one Downtown store that is missing this value.
+# Multiple other stores are missing 'localarea' values, but these are not Downtown.
+coffee_chains$localarea[coffee_chains$house == "490" &
+                          coffee_chains$street == "HORNBY ST"] <-
+  "Downtown"
+
+# How many businesses are closing and opening each year
+license_count_downtown_by_year <- coffee_chains %>%
+  filter(localarea == "Downtown") %>%
+  group_by(formatted_tradename, year) %>%
+  tally(name = "number_of_locations_downtown")
+
+license_count_not_downtown_by_year <- coffee_chains %>%
+  filter(localarea != "Downtown" | is.na(localarea)) %>%
+  group_by(formatted_tradename, year) %>%
+  tally(name = "numer_of_locations_not_downtown")
+
+license_count_by_year <- coffee_chains %>%
+  group_by(formatted_tradename, year) %>%
+  tally(name = "number_of_locations") %>%
+  inner_join(license_count_downtown_by_year) %>%
+  inner_join(license_count_not_downtown_by_year)
 
 # How are employee numbers changing each year
-employee_count <- coffee_franchises %>%
-  group_by(formatted_tradename, licenseyear) %>%
-  tally()
+employee_count <- coffee_chains %>%
+  group_by(formatted_tradename, year) %>%
+  tally(name = "employee_count")
 
 # How many consecutive years of operation for each location
 consecutive_years_of_operation <-
-  coffee_franchises %>% group_by(house, street, formatted_tradename) %>% distinct(folderyear) %>% tally()
+  coffee_chains %>%
+  group_by(house, street, formatted_tradename) %>%
+  distinct(year) %>%
+  tally(name = "consecutive_years_of_operation") %>%
+  arrange(formatted_tradename, consecutive_years_of_operation)
 
 # Filter only open business
-active_businesses <- coffee_franchises %>%
+open_coffee_chains <- coffee_chains %>%
   filter(expireddate > format(Sys.time(), '%Y-%m-%d'))
 
 # Grouping businesses by unique owners
-business_owner_count <- active_businesses %>%
-  group_by(businessname, formatted_tradename) %>% tally()
-
-# How many of each franchise are operating in the City of Vancouver
-franchise_count <- active_businesses %>%
-  group_by(formatted_tradename) %>% tally()
+business_owner_count <- open_coffee_chains %>%
+  group_by(businessname, formatted_tradename) %>%
+  tally(name = "numer_of_locations")  %>%
+  arrange(formatted_tradename, numer_of_locations)
 
 # How many employees are hired at each location
 premises_and_employee_count <-
-  active_businesses %>% group_by(formatted_tradename) %>%
+  open_coffee_chains %>%
+  group_by(formatted_tradename) %>%
   summarise(premises_count = n(),
             registered_employee_count = sum(as.numeric(numberofemployees)))
