4ad3ad33d9dedbddf502ebd880d668888f634a70
oisinBates <bates.oisin@gmail.com>
2023-08-20 17:14:50 -0700

    generating line plots

--- source/analytics.R
+++ source/analytics.R
@@ -1,7 +1,8 @@
-library(VancouvR)
 library(dplyr)
+library(ggplot2)
 library(readr)
 library(stringr)
+library(VancouvR)
 
 # Set API Option from .Renviron
 VancouverOpenDataApiKey <- Sys.getenv("vancouver_open_data_api_key")
@@ -57,6 +58,7 @@ coffee_chains$localarea[coffee_chains$house == "490" &
                           coffee_chains$street == "HORNBY ST"] <-
   "Downtown"
 
+### Start data wrangling/csv generation ###
 # How many businesses are closing and opening each year?
 license_count_downtown_by_year <- coffee_chains %>%
   filter(localarea == "Downtown") %>%
@@ -74,7 +76,8 @@ license_count_by_year <- coffee_chains %>%
   inner_join(license_count_downtown_by_year) %>%
   inner_join(license_count_not_downtown_by_year)
 
-write_csv(license_count_by_year, "output/raw_data/license_count_by_year.csv")
+write_csv(license_count_by_year,
+          "output/raw_data/license_count_by_year.csv")
 
 # How are employee and location numbers changing each year?
 premises_and_employee_count <-
@@ -87,7 +90,8 @@ premises_and_employee_count <-
     'average_number_employees_per_location'
   ), round, 2))
 
-write_csv(premises_and_employee_count, "output/raw_data/premises_and_employee_count.csv")
+write_csv(premises_and_employee_count,
+          "output/raw_data/premises_and_employee_count.csv")
 
 # How many consecutive years of operation for each location?
 consecutive_years_of_operation <-
@@ -97,13 +101,17 @@ consecutive_years_of_operation <-
   tally(name = "consecutive_years_of_operation") %>%
   arrange(formatted_tradename, consecutive_years_of_operation)
 
-write_csv(consecutive_years_of_operation, "output/raw_data/consecutive_years_of_operation.csv")
+write_csv(
+  consecutive_years_of_operation,
+  "output/raw_data/consecutive_years_of_operation.csv"
+)
 
 # Filter only open business
 open_coffee_chains <- coffee_chains %>%
   filter(expireddate > format(Sys.time(), '%Y-%m-%d'))
 
-write_csv(open_coffee_chains, "output/raw_data/open_coffee_chains.csv")
+write_csv(open_coffee_chains,
+          "output/raw_data/open_coffee_chains.csv")
 
 # How many unique owners are there?
 unique_business_owners <- open_coffee_chains %>%
@@ -111,4 +119,65 @@ unique_business_owners <- open_coffee_chains %>%
   tally(name = "numer_of_locations")  %>%
   arrange(formatted_tradename, numer_of_locations)
 
-write_csv(unique_business_owners, "output/raw_data/unique_business_owners.csv")
+write_csv(unique_business_owners,
+          "output/raw_data/unique_business_owners.csv")
+
+### End data wrangling/csv generation ###
+
+### Start visualization ###
+coffee_brand_colors <-
+  c(
+    blenz = "#5d4530",
+    jj_bean = "#fa002d",
+    mcdonalds = "#FFCC00",
+    starbucks = "#006241",
+    tim_hortons = "#C8102F"
+  )
+
+coffee_brand_labels <-
+  c("Blenz", "JJ Bean", "McDonald's", "Starbucks", "Tim Hortons")
+
+###### Start line plot generation ######
+ggplot(
+  license_count_by_year,
+  aes(
+    x = year,
+    y = number_of_locations,
+    color = formatted_tradename,
+    group = formatted_tradename
+  )
+) +
+  geom_line(linewidth = 1) +
+  scale_color_manual(name = "Store Name",
+                     values = coffee_brand_colors,
+                     labels = coffee_brand_labels) +
+  scale_y_continuous(breaks = pretty(license_count_by_year$number_of_locations, n = 20)) +
+  labs(title = "City of Vancouver Coffee Chains",
+       y = "Store Count",
+       x = "Year")
+
+ggsave("output/line_plots/coffee_chains_city_of_vancouver.png")
+
+ggplot(
+  license_count_by_year,
+  aes(
+    x = year,
+    y = number_of_locations_downtown,
+    color = formatted_tradename,
+    group = formatted_tradename
+  )
+) +
+  geom_line(linewidth = 1) +
+  scale_color_manual(name = "Store Name",
+                     values = coffee_brand_colors,
+                     labels = coffee_brand_labels) +
+  scale_y_continuous(breaks = pretty(license_count_by_year$number_of_locations_downtown, n = 15)) +
+  labs(title = "Downtown Vancouver Coffee Chains",
+       y = "Store Count",
+       x = "Year")
+
+ggsave("output/line_plots/coffee_chains_downtown_vancouver.png")
+###### End line plot generation ######
+
+
+### End visualization ###
