8a171b36f0616e7552d80849df0bd331ead3122d
oisinBates <bates.oisin@gmail.com>
2023-08-23 05:21:00 -0700

    generating interactive maps for 2017/2020/2023

--- source/mapping.R
+++ source/mapping.R
@@ -11,7 +11,7 @@ VancouverOpenDataApiKey <- Sys.getenv("vancouver_open_data_api_key")
 # Get business with active licenses
 # Escaped apostrophes break the API queries, so searching without them and applying additional filtering in Dplyr
 query_string =
-  "(businesstradename LIKE 'Blenz' OR businesstradename LIKE 'JJ Bean' OR businesstradename LIKE 'McDonald*' OR businesstradename LIKE 'Starbucks' OR businesstradename LIKE 'Tim Horton*' ) AND (folderyear='19' OR folderyear='23') AND status='Issued'"
+  "(businesstradename LIKE 'Blenz' OR businesstradename LIKE 'JJ Bean' OR businesstradename LIKE 'McDonald*' OR businesstradename LIKE 'Starbucks' OR businesstradename LIKE 'Tim Horton*' ) AND (businesstype='Ltd Service Food Establishment' OR businesstype='Restaurant Class 1' ) AND (folderyear='17' OR folderyear='20' OR folderyear='23') AND status='Issued'"
 licensed_coffee_businesses <-
   get_cov_data(dataset_id = "business-licences", where = query_string)
 
@@ -37,23 +37,21 @@ formatted_coffee_businesses <- licensed_coffee_businesses %>%
   )
 
 # Setting missing coordinates for '2501 Main Street'
-formatted_coffee_businesses$geom[formatted_coffee_businesses$licencenumber == "19-148691"] <-
+formatted_coffee_businesses$geom[formatted_coffee_businesses$house == "2501" &
+                                   formatted_coffee_businesses$street == "MAIN ST"] <-
   "{\"coordinates\": [-123.10139591801362, 49.26279740435901]}"
 # Setting missing coordinates for '1752 Commercial Drive'
-formatted_coffee_businesses$geom[formatted_coffee_businesses$licencenumber == "19-127622"] <-
+formatted_coffee_businesses$geom[formatted_coffee_businesses$house == "1752" &
+                                   formatted_coffee_businesses$street == "Commercial Dr"] <-
   "{\"coordinates\": [-123.06942508243132, 49.26885285075189]}"
 # Setting missing coordinates for '2517 Commercial Drive'
-formatted_coffee_businesses$geom[formatted_coffee_businesses$licencenumber == "19-149595"] <-
+formatted_coffee_businesses$geom[formatted_coffee_businesses$house == "2517" &
+                                   formatted_coffee_businesses$street == "Commercial Dr"] <-
   "{\"coordinates\": [-123.07014326074759, 49.262025289438824]}"
 
 
 formatted_coffee_businesses <- formatted_coffee_businesses %>%
   mutate(Long = as.numeric(sub(".*\\[(.*?),.*", "\\1", geom)), Lat = as.numeric(sub(".* (.*)\\].*", "\\1", geom)))
-
-formatted_coffee_businesses_2019 <-
-  formatted_coffee_businesses %>% filter(folderyear == "19")
-formatted_coffee_businesses_2023 <-
-  formatted_coffee_businesses %>% filter(folderyear == "23")
 
 
 coffee_icon_list <- iconList(
@@ -90,25 +88,36 @@ coffee_icon_list <- iconList(
   )
 )
 
+create_interactive_map <-
+  function(year,
+           coffee_icon_list,
+           formatted_coffee_businesses) {
+    # year <- "2020"
+    formatted_year = substr(year, 3, 4)
 
-mapped_business_locations_2019 <-
-  leaflet(formatted_coffee_businesses_2019) %>%
-  addTiles() %>%
-  # Select from coffee_icon_list based on formatted_coffee_businesses$formatted_tradename
-  addMarkers(icon = ~ coffee_icon_list[formatted_tradename])
-mapped_business_locations_2023 <-
-  leaflet(formatted_coffee_businesses_2023) %>%
-  addTiles() %>%
-  addMarkers(icon = ~ coffee_icon_list[formatted_tradename])
+    filtered_data <-
+      formatted_coffee_businesses %>% filter(folderyear == formatted_year)
 
-# Outputting the leaflet widget as html
-# Currently generating PNG images of the map via manual screenshot.
-## A solution like 'mapshot' could be utilised to automate this - https://r-spatial.github.io/mapview/reference/mapshot.html
-with_dir(
-  'output/interactive_maps',
-  saveWidget(mapped_business_locations_2019, file = "coffee_chain_locations2019.html")
-)
-with_dir(
-  'output/interactive_maps',
-  saveWidget(mapped_business_locations_2023, file = "coffee_chain_locations2023.html")
-)
+    mapped_business_locations <-
+      leaflet(filtered_data) %>%
+      addTiles() %>%
+      # Select from coffee_icon_list based on formatted_coffee_businesses$formatted_tradename
+      addMarkers(icon = ~ coffee_icon_list[formatted_tradename])
+
+    output_filename <-
+      paste0("coffee_chain_locations_", year, ".html")
+
+    # Outputting the leaflet widget as html
+    # Currently generating PNG images of the map via manual screenshot.
+    # A solution like 'mapshot' could be utilised to automate this - https://r-spatial.github.io/mapview/reference/mapshot.html
+    with_dir(
+      'output/interactive_maps',
+      saveWidget(mapped_business_locations, file = output_filename)
+    )
+
+
+  }
+
+create_interactive_map("2023", coffee_icon_list, formatted_coffee_businesses)
+create_interactive_map("2020", coffee_icon_list, formatted_coffee_businesses)
+create_interactive_map("2017", coffee_icon_list, formatted_coffee_businesses)
